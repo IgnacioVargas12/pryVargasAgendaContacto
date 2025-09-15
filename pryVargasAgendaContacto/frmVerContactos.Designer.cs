7508e79dab5650704d0513ab99000b840d5667e9
IgnacioVargas12 <nachoigna2007@gmail.com>
2025-09-15 20:46:53 -0300

    Agregue funcionalidad (completa)

--- pryVargasAgendaContacto/frmVerContactos.Designer.cs
+++ pryVargasAgendaContacto/frmVerContactos.Designer.cs
@@ -31,13 +31,16 @@
             lblDatos = new Label();
             btnSiguiente = new Button();
             btnAtras = new Button();
+            lblDatosIndividual = new Label();
+            lblListadoContactos = new Label();
+            lblContactosIndividual = new Label();
             SuspendLayout();
             // 
             // lblDatos
             // 
             lblDatos.BorderStyle = BorderStyle.Fixed3D;
             lblDatos.Font = new Font("Segoe UI", 12F);
-            lblDatos.Location = new Point(21, 40);
+            lblDatos.Location = new Point(21, 56);
             lblDatos.Name = "lblDatos";
             lblDatos.Size = new Size(280, 135);
             lblDatos.TabIndex = 0;
@@ -47,28 +50,63 @@
             // btnSiguiente
             // 
             btnSiguiente.Font = new Font("Segoe UI", 12F);
-            btnSiguiente.Location = new Point(155, 187);
+            btnSiguiente.Location = new Point(154, 374);
             btnSiguiente.Name = "btnSiguiente";
             btnSiguiente.Size = new Size(97, 37);
             btnSiguiente.TabIndex = 1;
             btnSiguiente.Text = "Siguiente";
             btnSiguiente.UseVisualStyleBackColor = true;
+            btnSiguiente.Click += btnSiguiente_Click_1;
             // 
             // btnAtras
             // 
             btnAtras.Font = new Font("Segoe UI", 12F);
-            btnAtras.Location = new Point(52, 187);
+            btnAtras.Location = new Point(51, 374);
             btnAtras.Name = "btnAtras";
             btnAtras.Size = new Size(97, 37);
             btnAtras.TabIndex = 2;
             btnAtras.Text = "Atras";
             btnAtras.UseVisualStyleBackColor = true;
+            btnAtras.Click += btnAtras_Click_1;
+            // 
+            // lblDatosIndividual
+            // 
+            lblDatosIndividual.BorderStyle = BorderStyle.Fixed3D;
+            lblDatosIndividual.Font = new Font("Segoe UI", 12F);
+            lblDatosIndividual.Location = new Point(21, 236);
+            lblDatosIndividual.Name = "lblDatosIndividual";
+            lblDatosIndividual.Size = new Size(280, 135);
+            lblDatosIndividual.TabIndex = 3;
+            lblDatosIndividual.Text = "---";
+            // 
+            // lblListadoContactos
+            // 
+            lblListadoContactos.AutoSize = true;
+            lblListadoContactos.Font = new Font("Segoe UI", 14F);
+            lblListadoContactos.Location = new Point(21, 26);
+            lblListadoContactos.Name = "lblListadoContactos";
+            lblListadoContactos.Size = new Size(184, 25);
+            lblListadoContactos.TabIndex = 4;
+            lblListadoContactos.Text = "Listado de contactos";
+            // 
+            // lblContactosIndividual
+            // 
+            lblContactosIndividual.AutoSize = true;
+            lblContactosIndividual.Font = new Font("Segoe UI", 14F);
+            lblContactosIndividual.Location = new Point(21, 211);
+            lblContactosIndividual.Name = "lblContactosIndividual";
+            lblContactosIndividual.Size = new Size(160, 25);
+            lblContactosIndividual.TabIndex = 5;
+            lblContactosIndividual.Text = "Listado individual";
             // 
             // frmVerContactos
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(329, 237);
+            ClientSize = new Size(329, 428);
+            Controls.Add(lblContactosIndividual);
+            Controls.Add(lblListadoContactos);
+            Controls.Add(lblDatosIndividual);
             Controls.Add(btnAtras);
             Controls.Add(btnSiguiente);
             Controls.Add(lblDatos);
@@ -76,6 +114,7 @@
             Text = "Listado contactos";
             Load += frmVerContactos_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -83,5 +122,8 @@
         private Label lblDatos;
         private Button btnSiguiente;
         private Button btnAtras;
+        private Label lblDatosIndividual;
+        private Label lblListadoContactos;
+        private Label lblContactosIndividual;
     }
 }
--- pryVargasAgendaContacto/frmVerContactos.cs
+++ pryVargasAgendaContacto/frmVerContactos.cs
@@ -19,6 +19,7 @@ namespace pryVargasAgendaContacto
 
         //Declaración variables
         int indice = 0;
+        int indice2 = 0;
         public string[] vecContactos2 = new string[5];
         string Resultado = "";
 
@@ -27,50 +28,57 @@ namespace pryVargasAgendaContacto
 
         }
 
-        //se encuentra el private void siguiente linea 56 (frmVerContactos.Designer.cs*)
-        //private void btnSiguiente_Click(object sender, EventArgs e)
-        //{
-        //    indice++;
-        //    lblDatos.Text = vecContactos2[indice];
-
-        //    if (vecContactos2.Length <= (indice + 1))
-        //    {
-        //        btnSiguiente.Enabled = false;
-        //    }
-
-        //    if (indice > 0)
-        //    {
-        //        btnAtras.Enabled = true;
-        //    }
-        //}
-
-        //se encuentra el private void atras en la linea 67 (frmVerContactos.Designer.cs*)
-        //private void btnAtras_Click(object sender, EventArgs e)
-        //{
-        //    indice--;
-        //    lblDatos.Text = vecContactos2[indice];
-
-        //    if (indice == 0)
-        //    {
-        //        btnAtras.Enabled = false;
-        //    }
-
-        //    if (indice < 5)
-        //    {
-        //        btnSiguiente.Enabled = true;
-        //    }
-        //}
-
         private void frmVerContactos_Load(object sender, EventArgs e)
         {
-            //lblDatos.Text = vecContactos2[0];
-            //btnAtras.Enabled = false;
-            while (indice < vecContactos2.Length && !string.IsNullOrEmpty(vecContactos2[indice])) 
+            lblDatosIndividual.Text = vecContactos2[0];
+            btnAtras.Enabled = false;
+            while (indice < vecContactos2.Length && !string.IsNullOrEmpty(vecContactos2[indice]))
             {
                 Resultado += vecContactos2[indice] + Environment.NewLine;
                 indice++;
             }
             lblDatos.Text = Resultado;
+        }
+
+        private void btnAtras_Click_1(object sender, EventArgs e)
+        {
+            indice2--;
+            lblDatosIndividual.Text = vecContactos2[indice2];
+
+            if (indice2 == 0)
+            {
+                btnAtras.Enabled = false;
+            }
+
+            if (indice2 < 5)
+            {
+                btnSiguiente.Enabled = true;
+            }
+        }
+
+        private void btnSiguiente_Click_1(object sender, EventArgs e)
+        {
+            indice2++;
+            lblDatosIndividual.Text = vecContactos2[indice2];
+
+            if (vecContactos2.Length <= (indice2 + 1))
+            {
+                btnSiguiente.Enabled = false;
+            }
+
+            if (vecContactos2[indice2 + 1] is not null )
+            {
+                btnSiguiente.Enabled = true;
+            }
+            else
+            {
+                btnSiguiente.Enabled =false;
+            }
+
+            if (indice2 >= 0)
+            {
+                btnAtras.Enabled = true;
+            }
         }
     }
 }
